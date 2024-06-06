a000c4b1d82f1c09509d9b685d704a38714dc01d
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-06 16:36:00 +0200

    Create t5_generation_template.sh

---
+++ sbatch/t5_generation_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A NAISS2024-5-148 -p alvis
+#SBATCH -t 8:00:00
+#SBATCH --gpus-per-node=A40:1
+
+export HF_DATASETS_CACHE=$TMPDIR
+export HF_HOME=$TMPDIR
+
+export CUDA_VISIBLE_DEVICES="0"
+
+echo "Output: $1"
+echo "Model: $2"
+echo "Finetuned model: $3"
+echo "Test set: $4"
+echo "Qlora: $5"
+
+python src/t5_generation.py --test_set $1
