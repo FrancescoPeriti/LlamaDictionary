b2fae772b9fc4479872e6716cd70ae615a13b97d
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-06 15:49:42 +0200

    Create generation_template.sh

---
+++ sbatch/generation_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A NAISS2024-5-148 -p alvis
+#SBATCH -t 9:00:00
+#SBATCH --gpus-per-node=A100fat:1
+
+export HF_DATASETS_CACHE=$TMPDIR
+export HF_HOME=$TMPDIR
+
+export CUDA_VISIBLE_DEVICES="0"
+
+echo "Output: $1"
+echo "Model: $2"
+echo "Finetuned model: $3"
+echo "Test set: $4"
+echo "Qlora: $5"
+
+python src/generation.py --output $1 --model $2 --finetuned_model $3 --test_set $4 --batch_size 8 --hf_token [YOUR_HF_TOKEN] --hf_cache_dir $HF_HOME --qlora $5
