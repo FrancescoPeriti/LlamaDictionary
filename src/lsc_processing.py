41a7bf08eb4fd98ed4ce1e10aa3d8836ce91c987
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-11 23:11:58 +0200

    Update lsc_processing.py

--- src/lsc_processing.py
+++ src/lsc_processing.py
@@ -2,6 +2,24 @@ import os
 import csv
 import pandas as pd
 from pathlib import Path
+from datasets import load_datasets
+
+# we tried by quotig the target word as the pos infomation was available
+def processing(example, length=None):
+    try:
+        start, end = example['indexes_target_token'].split(':')
+        start, end = int(start), int(end)
+    except:
+        start = example['indexes_target_token'].split(':')[0]
+        end = len(example['example'])
+
+    example['example'] = example['example'][:int(start)] + '"' + example['example'][int(start):int(end)] + '"' + example['example'][int(end):]
+
+    if length is None:
+        return example['example']
+    else:
+        return example['example'][max(0,int(start)-length):int(end)+length] # limit context length
+
 
 targets = sorted(os.listdir('dwug_en/data/'))
 
@@ -16,9 +34,27 @@ for target in targets:
     target = target.replace('_nn', '').replace('_vb', '')
     df['target'] = target
     dfs[target] = df
-    dfs[target].to_json(orient='records', lines=True)
     Path('dwug_en/wsi/').mkdir(parents=True, exist_ok=True)
+    Path('dwug_en25/wsi/').mkdir(parents=True, exist_ok=True)
+    Path('dwug_en50/wsi/').mkdir(parents=True, exist_ok=True)
+    Path('dwug_en75/wsi/').mkdir(parents=True, exist_ok=True)
+    Path('dwug_en100/wsi/').mkdir(parents=True, exist_ok=True)
     dfs[target].to_json(f'dwug_en/wsi/{target}.jsonl', orient='records', lines=True)
+    test_dataset = load_dataset('json', data_files=f'dwug_en/wsi/{target}.jsonl', split='train')
+    df['example'] = test_dataset.map(lambda x: {'example': processing(x)})['example']
+    df.to_json(f'dwug_en/wsi/{target}.jsonl', orient='records', lines=True)
+    test_dataset = load_dataset('json', data_files=f'dwug_en/wsi/{target}.jsonl', split='train')
+    df['example'] = test_dataset.map(lambda x: {'example': processing(x, 25)})['example']
+    df.to_json(f'dwug_en/wsi25/{target}.jsonl', orient='records', lines=True)
+    test_dataset = load_dataset('json', data_files=f'dwug_en/wsi/{target}.jsonl', split='train')
+    df['example'] = test_dataset.map(lambda x: {'example': processing(x, 50)})['example']
+    df.to_json(f'dwug_en/wsi50/{target}.jsonl', orient='records', lines=True)
+    test_dataset = load_dataset('json', data_files=f'dwug_en/wsi/{target}.jsonl', split='train')
+    df['example'] = test_dataset.map(lambda x: {'example': processing(x, 75)})['example']
+    df.to_json(f'dwug_en/wsi75/{target}.jsonl', orient='records', lines=True)
+    test_dataset = load_dataset('json', data_files=f'dwug_en/wsi/{target}.jsonl', split='train')
+    df['example'] = test_dataset.map(lambda x: {'example': processing(x, 100)})['example']
+    df.to_json(f'dwug_en/wsi100/{target}.jsonl', orient='records', lines=True)
     
 dfs_labels = dict()
 for target in targets:
