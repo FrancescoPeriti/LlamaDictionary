72893ac1bbf8f21f192e20943154c033caa66601
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-13 01:14:02 +0200

    Update wic_evaluation.sh

--- sbatch/wic_evaluation.sh
+++ sbatch/wic_evaluation.sh
@@ -5,14 +5,12 @@
 
 export HF_DATASETS_CACHE=$TMPDIR
 export HF_HOME=$TMPDIR
-
 export CUDA_VISIBLE_DEVICES="0"
 
-
+# params used in paper: model:all-distilroberta-v1
 models=("all-mpnet-base-v2" "multi-qa-mpnet-base-dot-v1" "all-distilroberta-v1" "all-MiniLM-L12-v2" "multi-qa-distilbert-cos-v1" "all-MiniLM-L6-v2" "multi-qa-MiniLM-L6-cos\
 -v1" "paraphrase-multilingual-mpnet-base-v2" "paraphrase-albert-small-v2" "paraphrase-multilingual-MiniLM-L12-v2" "paraphrase-MiniLM-L3-v2" "distiluse-base-multilingual-ca\
 sed-v1" "distiluse-base-multilingual-cased-v2")
-
 
 for model in "${models[@]}"; do
     python src/wic_evaluation.py -m "$model"
