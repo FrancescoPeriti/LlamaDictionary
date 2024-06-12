2f81b79ba412723acaee02905074e88b79bfb5f1
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-13 00:56:31 +0200

    Update wic_evaluation.py

--- src/wic_evaluation.py
+++ src/wic_evaluation.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score, roc_curve
 
 def set_threshold(y_true: np.array, y: np.array) -> float:
     """
-    Find the threshold that maximize accuracy on the Dev set.
+    Find the threshold that maximize the area under the curve.
 
     Args:
         y(np.array): array containing predicted values
@@ -33,22 +33,27 @@ def set_threshold(y_true: np.array, y: np.array) -> float:
     return max_threshold
     
 
+import argparse
+parser = argparse.ArgumentParser(prog='WiC', description="WiC evaluation")
+parser.add_argument('-m', '--model', type=str, default="sentence-transformers/all-distilroberta-v1")
+args = parser.parse_args()
+
 # load model
-model = SentenceTransformer("sentence-transformers/all-distilroberta-v1")
+model = SentenceTransformer(args.model)
 
 # load examples
 dev_examples = [json.loads(i) for i in open('wic/dev/dev.data.txt').readlines()]
 test_examples = [json.loads(i)  for i in open('wic/test/test.data.txt').readlines()]
 
-results = list()
-for folder_answers in ['wic-lora-anwers', 'wic-t5-answers']:
-    print('--', folder_answers.upper(), '--')
-    
+for folder_answers in ['wic-lora-answers', 'wic-t5-answers']:
+    results = list()
     for ft_model_name in os.listdir(folder_answers):
         # load model answers
-        dev_answers = [i.strip() for i in open(f'{folder_answers}/{ft_model_name}/dev.data.txt').readlines()]
-        test_answers = [i.strip() for i in open(f'{folder_answers}/{ft_model_name}/test.data.txt').readlines()]
-
+        dev_answers = [" ".join([t for t in i.strip().split() if t not in ';.,()']) for i in open(f'{folder_answers}/{ft_model_name}/dev.data.txt').readlines()]
+        #dev_answers = [i.strip() for i in open(f'{folder_answers}/{ft_model_name}/dev.data.txt').readlines()] # still good result
+        test_answers = [" ".join([t for t in i.strip().split() if t not in ';.,()']) for i in open(f'{folder_answers}/{ft_model_name}/test.data.txt').readlines()]
+        #test_answers = [i.strip() for i in open(f'{folder_answers}/{ft_model_name}/test.data.txt').readlines()] # still good result
+        
         # load labels
         dev_labels = [int(i.strip() == 'T') for i in open('wic/dev/dev.gold.txt').readlines()]
         test_labels = [int(i.strip() == 'T') for i in open('wic/test/test.gold.txt').readlines()]
@@ -58,12 +63,12 @@ for folder_answers in ['wic-lora-anwers', 'wic-t5-answers']:
         sim_dev = model.similarity(emb_dev, emb_dev) 
         emb_test = model.encode(test_answers)
         sim_test = model.similarity(emb_test, emb_test)
-
+        
         # Dev and Test sets
         dev = [dict(gloss1=dev_answers[i], gloss2=dev_answers[i + 1],
                     sim=sim_dev[i][i+1], example1=dev_examples[i]['example'], example2=dev_examples[i+1]['example'],
                     label=dev_labels[i//2]) for i in range(0, len(dev_answers) - 1, 2)]
-    
+        
         test = [dict(gloss1=test_answers[i], gloss2=test_answers[i + 1], sim=sim_test[i][i+1],
                      example1=test_examples[i]['example'], example2=test_examples[i+1]['example'],
                      label=test_labels[i//2]) for i in range(0, len(test_answers) - 1, 2)]
@@ -77,9 +82,9 @@ for folder_answers in ['wic-lora-anwers', 'wic-t5-answers']:
 
         # compute accuracy
         acc_test = accuracy_score(np.array(test_labels), test_preds)
-        results.append(dict(model=ft_model_name, accuracy=round(acc_test, 3)))
-        print('MODEL:', ft_model_name, ' - ACCURACY:', round(acc_test, 3))
+        results.append(dict(sbert=args.model, model=ft_model_name, accuracy=round(acc_test, 3)))
+        #print('MODEL:', ft_model_name, ' - ACCURACY:', round(acc_test,3))
 
-with open('wic-results.jsonl', mode='w', encoding='utf-8') as f:
-    for row in results:
-        f.write(json.dumps(row)+'\n')
+    with open('wic-results.jsonl', mode='a+', encoding='utf-8') as f:
+        for row in results:
+            f.write(json.dumps(row)+'\n')
