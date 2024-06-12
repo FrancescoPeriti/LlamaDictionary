337935058711d29185113b9b005f395596fae850
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-12 21:45:08 +0200

    Update wic_evaluation.sh

--- sbatch/wic_evaluation.sh
+++ sbatch/wic_evaluation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #SBATCH -A NAISS2024-5-148 -p alvis
-#SBATCH -t 1:00:00
-#SBATCH --gpus-per-node=A40:1
+#SBATCH -t 5:00:00
+#SBATCH --gpus-per-node=A100:1
 
 export HF_DATASETS_CACHE=$TMPDIR
 export HF_HOME=$TMPDIR
+
 export CUDA_VISIBLE_DEVICES="0"
 
-python src/wic_evaluation.py
+
+models=("all-mpnet-base-v2" "multi-qa-mpnet-base-dot-v1" "all-distilroberta-v1" "all-MiniLM-L12-v2" "multi-qa-distilbert-cos-v1" "all-MiniLM-L6-v2" "multi-qa-MiniLM-L6-cos\
+-v1" "paraphrase-multilingual-mpnet-base-v2" "paraphrase-albert-small-v2" "paraphrase-multilingual-MiniLM-L12-v2" "paraphrase-MiniLM-L3-v2" "distiluse-base-multilingual-ca\
+sed-v1" "distiluse-base-multilingual-cased-v2")
+
+
+for model in "${models[@]}"; do
+    python src/wic_evaluation.py -m "$model"
+done
