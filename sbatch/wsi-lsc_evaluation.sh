806f1dab74396110c3db44b2b7be9a6e89de42a4
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2024-06-13 01:13:26 +0200

    Update wsi-lsc_evaluation.sh

--- sbatch/wsi-lsc_evaluation.sh
+++ sbatch/wsi-lsc_evaluation.sh
@@ -5,13 +5,12 @@
 
 export HF_DATASETS_CACHE=$TMPDIR
 export HF_HOME=$TMPDIR
-
 export CUDA_VISIBLE_DEVICES="0"
 
-lengths=(2 3 4)
-models=("all-mpnet-base-v2" "multi-qa-mpnet-base-dot-v1" "all-distilroberta-v1" "all-MiniLM-L12-v2" "multi-qa-distilbert-cos-v1" "all-MiniLM-L6-v2" "multi-qa-MiniLM-L6-cos\
--v1" "paraphrase-multilingual-mpnet-base-v2" "paraphrase-albert-small-v2" "paraphrase-multilingual-MiniLM-L12-v2" "paraphrase-MiniLM-L3-v2" "distiluse-base-multilingual-ca\
-sed-v1" "distiluse-base-multilingual-cased-v2")
+# params used in paper: length:1, model:all-distilroberta-v1
+
+lengths=(1 2 3 4) 
+models=("all-mpnet-base-v2" "multi-qa-mpnet-base-dot-v1" "all-distilroberta-v1" "all-MiniLM-L12-v2" "multi-qa-distilbert-cos-v1" "all-MiniLM-L6-v2" "multi-qa-MiniLM-L6-cos-v1" "paraphrase-multilingual-mpnet-base-v2" "paraphrase-albert-small-v2" "paraphrase-multilingual-MiniLM-L12-v2" "paraphrase-MiniLM-L3-v2" "distiluse-base-multilingual-cased-v1" "distiluse-base-multilingual-cased-v2")
 
 for l in "${lengths[@]}"; do
     for model in "${models[@]}"; do
